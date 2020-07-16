d16b7a2051bd9f894a5275fec57c8cb9afa5f169
Karl Steven <54846911+Avicity7@users.noreply.github.com>
2020-07-16 10:10:57 +0800

    Update subtraction.py

--- operators/subtraction/subtraction.py
+++ operators/subtraction/subtraction.py
@@ -7,49 +7,26 @@ In fact, you're able to subtract almost all common types of Python together!
 """
 
 # Subtraction with integers
-integerA = 1
-integerB = 2
-integerTotal = integerA + integerB  
-print(integerTotal)                 # Prints out 3
+integerA = 2
+integerB = 1
+integerTotal = integerA - integerB  
+print(integerTotal)                 # Prints out 1
 
 # Subtraction with floats
 floatA = 1.5
 floatB = 2.5
-floatTotal = floatA + floatB
-print(floatTotal)                   # Prints out 4.0
+floatTotal = floatA - floatB
+print(floatTotal)                   # Prints out 1
 
 # Subtraction with floats and integers
 """
 Note:
 As of Python 3+, integers will be automatically converted to floats to give you the expected value.
 """
-numberTotal = integerA + floatB
-print(numberTotal)                  # Prints out 4.5
+numberTotal = integerA - floatB
+print(numberTotal)                  # Prints out -0.5
 
-# Subtraction with lists
-listA = ['hello', 'world']
-listB = ['welcome', 'to', 'GitHub']
-listTotal = listA + listB
-print(listTotal)                    # Prints out ['hello', 'world', 'welcome', 'to', 'GitHub']
+# You cannot subtract lists.
 
 # You cannot add dictionaries together.
 
-"""
-Python also supports adding values to variables.
-Note that the type of what you're trying to add must match the type of the original variable.
-Only integers and floats are excluded from this rule.
-"""
-myString = 'Hello '
-myString += 'world'
-print(myString)                     # Prints out Hello world
-
-myInt = 50
-myInt += 10
-print(myInt)                        # Prints out 60
-
-myFloat = 5.5
-myFloat += 4.0
-print(myFloat)                      # Prints out 9.5
-
-myInt += myFloat
-print(myInt)                        # Prints out 69.5
